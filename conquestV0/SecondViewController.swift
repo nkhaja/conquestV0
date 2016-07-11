3edaaecda7388566e724b05b9a6c1e5375578f45
nkhaja <nbl.khaja@gmail.com>
2016-07-11 09:13:44 -0700

    Initial Commit

---
+++ conquestV0/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  conquestV0
+//
+//  Created by Nabil K on 2016-07-11.
+//  Copyright © 2016 Nabil. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
